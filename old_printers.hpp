ac026a337cb6e7851d09d2b492d09e3890cb3d49
Govind Sahai <gsiitbhu@gmail.com>
2015-07-13 16:31:52 +0530

    New printers added

---
+++ old_printers.hpp
@@ -0,0 +1,320 @@
+// Printers for Containers defined here
+
+#pragma once
+
+//#include "stdafx.h"
+//#include "first.hpp"
+#include <bits/stdc++.h>
+
+namespace mydetails
+{
+	// Helper for std::tuple<> and std::pair<>
+	template<size_t N> struct TuplePrinter
+	{
+		template<typename... Types>
+		static void print(const std::tuple<Types...>& ar)
+		{
+			TuplePrinter<N - 1>::print(ar);
+			typedef typename std::remove_const<
+				typename std::remove_reference<
+				decltype(std::get<N - 1>(ar))>::type>::type TT;
+			if (std::is_same<std::string, TT>::value)
+				std::cout << "\"" << std::get<N - 1>(ar) << "\",";
+			else
+				std::cout << std::get<N - 1>(ar) << ",";
+		}
+	};
+	template<> struct TuplePrinter<0>
+	{
+		template<typename... Types>
+		static void print(const std::tuple<Types...>&)
+		{
+			//Nothing here
+		}
+	};
+
+	// printer for std::pair<T1,T2> : Checked
+	template<typename T1,
+		typename T2> std::ostream&
+		operator<<(std::ostream& xx, const std::pair<T1, T2>& pr)
+	{
+		constexpr size_t SZ = 2;
+		std::cout << "pr" << SZ << "(";
+		TuplePrinter<SZ>::print(std::make_tuple(pr.first, pr.second));
+		std::cout << "\b)";
+		return xx;
+	}
+
+	template<typename... Types> std::ostream&
+		operator<<(std::ostream& xx,
+			const std::tuple<Types...>& ar)
+	{
+		constexpr size_t SZ = std::tuple_size<
+			std::tuple<Types... >> ::value;
+		std::cout << "tp" << SZ << "(";
+		TuplePrinter<SZ>::print(ar);
+		std::cout << "\b)";
+		return xx;
+	}
+
+	// printer for std::vector<T> : Checked
+	template<typename T> std::ostream&
+		operator<<(std::ostream& xx, const std::vector<T>& ar)
+	{
+		std::cout << "vec" << ar.size() << "[";
+		for (const auto& x : ar)
+		{
+			typedef typename std::remove_const<
+				typename std::remove_reference<decltype(x)>::type>::type TT;
+			if (std::is_same<std::string, TT>::value)
+				std::cout << "\"" << x << "\",";
+			else
+				std::cout << x << ",";
+		}
+		if (ar.size() > 0)
+			std::cout << "\b";
+		std::cout << "]";
+		return xx;
+	}
+
+	// printer for C-array
+	template<typename T, size_t N>
+	std::enable_if_t<!(std::is_same<char, T>::value ||
+		std::is_same<wchar_t, T>::value), std::ostream&>
+		operator<<(std::ostream& xx, const T(&ar)[N])
+	{
+		std::cout << "carr" << N << "[";
+		for (const auto& x : ar)
+		{
+			typedef typename std::remove_const<
+				typename std::remove_reference<decltype(x)>::type>::type TT;
+			if (std::is_same<std::string, TT>::value)
+				std::cout << "\"" << x << "\",";
+			else
+				std::cout << x << ",";
+		}
+		std::cout << "\b]";
+		return xx;
+	}
+
+	template<typename T,
+		typename S = typename std::iterator_traits<T>::reference>
+		std::enable_if_t<!(std::is_pointer<T>::value), std::ostream&>
+		operator<<(std::ostream& xx, const T& it)
+	{
+		if (std::is_const<std::remove_reference_t<S>>::value)
+			std::cout << "cit[";
+		else
+			std::cout << "it[";
+		std::cout << *it;
+		std::cout << "]";
+		return xx;
+	}
+
+	template<typename T,
+		typename S = typename std::iterator_traits<T>::reference>
+		std::enable_if_t<std::is_pointer<T>::value, std::ostream&>
+		operator<<(std::ostream& xx, const T& it)
+	{
+		if (std::is_const<std::remove_reference_t<S>>::value)
+			std::cout << "cptr[";
+		else
+			std::cout << "ptr[";
+		std::cout << *it;
+		std::cout << "]";
+		return xx;
+	}
+
+	// printer for std::array<T, N> : Checked
+	template<typename T,
+		size_t N> std::ostream&
+		operator<<(std::ostream& xx, const std::array<T, N>& ar)
+	{
+		std::cout << "arr" << N << "[";
+		for (const auto& x : ar)
+		{
+			typedef typename std::remove_const<
+				typename std::remove_reference<decltype(x)>::type>::type TT;
+			if (std::is_same<std::string, TT>::value)
+				std::cout << "\"" << x << "\",";
+			else
+				std::cout << x << ",";
+		}
+		if (ar.size() > 0)
+			std::cout << "\b";
+		std::cout << "]";
+		return xx;
+	}
+
+	// printer for std::list<T> : Checked
+	template<typename T> std::ostream&
+		operator<<(std::ostream& xx, const std::list<T>& ar)
+	{
+		std::cout << "lst" << ar.size() << "[";
+		for (const auto& x : ar)
+		{
+			typedef typename std::remove_const<
+				typename std::remove_reference<decltype(x)>::type>::type TT;
+			if (std::is_same<std::string, TT>::value)
+				std::cout << "\"" << x << "\",";
+			else
+				std::cout << x << ",";
+		}
+		if (ar.size() > 0)
+			std::cout << "\b";
+		std::cout << "]";
+		return xx;
+	}
+
+	// printer for std::unordered_set<T> : Checked
+	template<typename T> std::ostream&
+		operator<<(std::ostream& xx, const std::unordered_set<T>& ar)
+	{
+		std::cout << "uset" << ar.size() << "{";
+		for (const auto& x : ar)
+		{
+			typedef typename std::remove_const<
+				typename std::remove_reference<decltype(x)>::type>::type TT;
+			if (std::is_same<std::string, TT>::value)
+				std::cout << "\"" << x << "\",";
+			else
+				std::cout << x << ",";
+		}
+		if (ar.size() > 0)
+			std::cout << "\b";
+		std::cout << "}";
+		return xx;
+	}
+
+	// printer for std::set<T>
+	template<typename T> std::ostream&
+		operator<<(std::ostream& xx, const std::set<T>& ar)
+	{
+		std::cout << "set" << ar.size() << "{";
+		for (const auto& x : ar)
+		{
+			typedef typename std::remove_const<
+				typename std::remove_reference<decltype(x)>::type>::type TT;
+			if (std::is_same<std::string, TT>::value)
+				std::cout << "\"" << x << "\",";
+			else
+				std::cout << x << ",";
+		}
+		if (ar.size() > 0)
+			std::cout << "\b";
+		std::cout << "}";
+		return xx;
+	}
+
+	// printer for std::unordered_multiset<T>
+	template<typename T> std::ostream&
+		operator<<(std::ostream& xx,
+			const std::unordered_multiset<T>& ar)
+	{
+		std::cout << "umset" << ar.size() << "{";
+		for (const auto& x : ar)
+		{
+			typedef typename std::remove_const<
+				typename std::remove_reference<decltype(x)>::type>::type TT;
+			if (std::is_same<std::string, TT>::value)
+				std::cout << "\"" << x << "\",";
+			else
+				std::cout << x << ",";
+		}
+		if (ar.size() > 0)
+			std::cout << "\b";
+		std::cout << "}";
+		return xx;
+	}
+
+	// printer for std::multiset<T>
+	template<typename T> std::ostream&
+		operator<<(std::ostream& xx, const std::multiset<T>& ar)
+	{
+		std::cout << "mset" << ar.size() << "{";
+		for (const auto& x : ar)
+		{
+			typedef typename std::remove_const<
+				typename std::remove_reference<decltype(x)>::type>::type TT;
+			if (std::is_same<std::string, TT>::value)
+				std::cout << "\"" << x << "\",";
+			else
+				std::cout << x << ",";
+		}
+		if (ar.size() > 0)
+			std::cout << "\b";
+		std::cout << "}";
+		return xx;
+	}
+
+	// printer for std::map<T1, T2> : Checked
+	template<typename T1, typename T2> std::ostream&
+		operator<<(std::ostream& xx, const std::map<T1, T2>& ar)
+	{
+		std::cout << "map" << ar.size() << "[";
+		for (const auto& x : ar)
+			std::cout << x << ",";
+		if (ar.size() > 0)
+			std::cout << "\b";
+		std::cout << "]";
+		return xx;
+	}
+
+	// printer for std::unordered_map<T1, T2>
+	template<typename T1, typename T2> std::ostream&
+		operator<<(std::ostream& xx, const std::unordered_map<T1, T2>& ar)
+	{
+		std::cout << "umap" << ar.size() << "[";
+		for (const auto& x : ar)
+			std::cout << x << ",";
+		if (ar.size() > 0)
+			std::cout << "\b";
+		std::cout << "]";
+		return xx;
+	}
+
+	// printer for std::multimapmap<T1, T2>
+	template<typename T1, typename T2> std::ostream&
+		operator<<(std::ostream& xx, const std::multimap<T1, T2>& ar)
+	{
+		std::cout << "mmap" << ar.size() << "[";
+		for (const auto& x : ar)
+			std::cout << x << ",";
+		if (ar.size() > 0)
+			std::cout << "\b";
+		std::cout << "]";
+		return xx;
+	}
+
+	// printer for std::unordered_multimap<T1, T2>
+	template<typename T1, typename T2> std::ostream&
+		operator<<(std::ostream& xx,
+			const std::unordered_multimap<T1, T2>& ar)
+	{
+		std::cout << "ummap" << ar.size() << "[";
+		for (const auto& x : ar)
+			std::cout << x << ",";
+		if (ar.size() > 0)
+			std::cout << "\b";
+		std::cout << "]";
+		return xx;
+	}
+
+	// printer for std::shared_ptr<T>
+	template<typename T> std::ostream&
+		operator<<(std::ostream& xx,
+			const std::shared_ptr<T>& sp)
+	{
+		std::cout << "sp[" << *sp << "]";
+		return xx;
+	}
+
+	// printer for std::unique_ptr<T>
+	template<typename T> std::ostream&
+		operator<<(std::ostream& xx,
+			const std::unique_ptr<T>& up)
+	{
+		std::cout << "up[" << *up << "]";
+		return xx;
+	}
+}
--- printers.hpp
+++ printers.hpp
@@ -1,319 +1,289 @@
-// Printers for Containers defined here
+// New Printers
 
 #pragma once
 
-#include "stdafx.h"
-#include "first.hpp"
+#include <bits/stdc++.h>
 
-namespace mydetails
+namespace printers
 {
-	// Helper for std::tuple<> and std::pair<>
-	template<size_t N> struct TuplePrinter
-	{
-		template<typename... Types>
-		static void print(const std::tuple<Types...>& ar)
-		{
-			TuplePrinter<N - 1>::print(ar);
-			typedef typename std::remove_const<
-				typename std::remove_reference<
-				decltype(std::get<N - 1>(ar))>::type>::type TT;
-			if (std::is_same<std::string, TT>::value)
-				std::cout << "\"" << std::get<N - 1>(ar) << "\",";
-			else
-				std::cout << std::get<N - 1>(ar) << ",";
-		}
-	};
-	template<> struct TuplePrinter<0>
-	{
-		template<typename... Types>
-		static void print(const std::tuple<Types...>&)
-		{
-			//Nothing here
-		}
-	};
+  namespace Helpers
+  {
+    std::string delimiter = ", ";
 
-	// printer for std::pair<T1,T2> : Checked
-	template<typename T1,
+    std::unordered_map<std::string, std::string> Names =
+    {
+      {"array", "arr"},
+      {"carray", "carr"},
+      {"const_iterator", "citr"},
+      {"const_pointer", "cptr"},
+      {"iterator", "itr"},
+      {"list", "lst"},
+      {"map", "map"},
+      {"multimap", "mmap"},
+      {"multiset", "mset"},
+      {"pair", "pr"},
+      {"pointer", "ptr"},
+      {"set", "set"},
+      {"shared_ptr", "sp"},
+      {"tuple", "tp"},
+      {"unique_ptr", "up"},
+      {"unordered_map", "umap"},
+      {"unordered_multimap", "ummap"},
+      {"unordered_multiset", "umset"},
+      {"unordered_set", "uset"},
+      {"vector", "vec"},
+    };
+
+    template<size_t N>
+    struct TuplePrinter
+    {
+    	template<typename... Types>
+      static void print(const std::tuple<Types...>& ar)
+      {
+      	TuplePrinter<N - 1>::print(ar);
+      	typedef typename std::remove_const<
+      		typename std::remove_reference<
+      		  decltype(std::get<N - 1>(ar))>::type>::type TT;
+      	if (std::is_same<std::string, TT>::value)
+      		std::cout << "\"" << std::get<N - 1>(ar) << "\"" << delimiter;
+      	else
+      		std::cout << std::get<N - 1>(ar) << delimiter;
+      }
+    };
+
+    template<>
+    struct TuplePrinter<0>
+    {
+    	template<typename... Types>
+    	static void print(const std::tuple<Types...>&) { }
+    };
+
+    class Printer
+    {
+    public:
+      virtual ~Printer() { }
+      virtual void printOpenBrace() = 0;
+      virtual void printEndBrace() = 0;
+
+      template<typename T>
+      void printElementsCont(const T& ar, std::string&& _name, size_t N)
+      {
+        std::cout << Names[_name] << N;
+        printOpenBrace();
+        for (const auto& x : ar)
+    		{
+    			typedef typename std::remove_const_t<
+    				typename std::remove_reference_t<decltype(x)>> TT;
+    			if (std::is_same<std::string, TT>::value)
+    				std::cout << "\"" << x << "\"" << delimiter;
+    			else
+    				std::cout << x << delimiter;
+    		}
+        if(N)
+          for(size_t i = 0; i < delimiter.size(); ++i)
+            std::cout << '\b';
+        printEndBrace();
+        return;
+      }
+
+      template<typename T>
+      void printElementsPtr(const T& ptr, std::string&& _name)
+      {
+        std::cout << Names[_name];
+        printOpenBrace();
+        typedef typename std::remove_const_t<
+          typename std::remove_reference_t<decltype(*ptr)>> TT;
+        if (std::is_same<std::string, TT>::value)
+          std::cout << "\"" << *ptr << "\"";
+        else
+          std::cout << *ptr;
+        printEndBrace();
+        return;
+      }
+
+      template<typename... TArgs>
+      void printElementsTup(const std::tuple<TArgs...>& tup, std::string&& _name)
+      {
+        constexpr size_t SZ = std::tuple_size<
+    			std::tuple<TArgs... >> ::value;
+        std::cout << Names[_name] << SZ;
+    		printOpenBrace();
+    		TuplePrinter<SZ>::print(tup);
+        for(size_t i = 0; i < delimiter.size(); ++i)
+    		  std::cout << '\b';
+        printEndBrace();
+      }
+    };
+
+    class BoxBraces : public Printer
+    {
+    public:
+      virtual void printOpenBrace() final { std::cout << "["; }
+      virtual void printEndBrace() final { std::cout << "]"; }
+    };
+
+    class CurlyBraces : public Printer
+    {
+    public:
+      virtual void printOpenBrace() final { std::cout << "{"; }
+      virtual void printEndBrace()  final { std::cout << "}"; }
+    };
+
+    class ParaBraces : public Printer
+    {
+    public:
+      virtual void printOpenBrace() final { std::cout << "("; }
+      virtual void printEndBrace()  final { std::cout << ")"; }
+    };
+
+    class AngleBraces : public Printer
+    {
+    public:
+      virtual void printOpenBrace() final { std::cout << "<"; }
+      virtual void printEndBrace()  final { std::cout << ">"; }
+    };
+  }
+  Helpers::BoxBraces    boxBraces;
+  Helpers::CurlyBraces  curlyBraces;
+  Helpers::ParaBraces   paraBraces;
+  Helpers::AngleBraces  angleBraces;
+
+  template<typename T1,
 		typename T2> std::ostream&
 		operator<<(std::ostream& xx, const std::pair<T1, T2>& pr)
 	{
-		constexpr size_t SZ = 2;
-		std::cout << "pr" << SZ << "(";
-		TuplePrinter<SZ>::print(std::make_tuple(pr.first, pr.second));
-		std::cout << "\b)";
+    curlyBraces.printElementsTup(std::make_tuple(pr.first, pr.second), "pair");
 		return xx;
 	}
 
-	template<typename... Types> std::ostream&
+  template<typename... Types> std::ostream&
 		operator<<(std::ostream& xx,
-			const std::tuple<Types...>& ar)
+			const std::tuple<Types...>& tup)
 	{
-		constexpr size_t SZ = std::tuple_size<
-			std::tuple<Types... >> ::value;
-		std::cout << "tp" << SZ << "(";
-		TuplePrinter<SZ>::print(ar);
-		std::cout << "\b)";
+		curlyBraces.printElementsTup(tup, "tuple");
 		return xx;
 	}
 
-	// printer for std::vector<T> : Checked
-	template<typename T> std::ostream&
+  template<typename T> std::ostream&
 		operator<<(std::ostream& xx, const std::vector<T>& ar)
 	{
-		std::cout << "vec" << ar.size() << "[";
-		for (const auto& x : ar)
-		{
-			typedef typename std::remove_const<
-				typename std::remove_reference<decltype(x)>::type>::type TT;
-			if (std::is_same<std::string, TT>::value)
-				std::cout << "\"" << x << "\",";
-			else
-				std::cout << x << ",";
-		}
-		if (ar.size() > 0)
-			std::cout << "\b";
-		std::cout << "]";
+    boxBraces.printElementsCont(ar, "vector", ar.size());
 		return xx;
 	}
 
-	// printer for C-array
-	template<typename T, size_t N>
+  template<typename T, size_t N>
 	std::enable_if_t<!(std::is_same<char, T>::value ||
 		std::is_same<wchar_t, T>::value), std::ostream&>
 		operator<<(std::ostream& xx, const T(&ar)[N])
 	{
-		std::cout << "carr" << N << "[";
-		for (const auto& x : ar)
-		{
-			typedef typename std::remove_const<
-				typename std::remove_reference<decltype(x)>::type>::type TT;
-			if (std::is_same<std::string, TT>::value)
-				std::cout << "\"" << x << "\",";
-			else
-				std::cout << x << ",";
-		}
-		std::cout << "\b]";
-		return xx;
+    boxBraces.printElementsCont(ar, "carray", N);
+    return xx;
 	}
 
-	template<typename T,
-		typename S = typename std::iterator_traits<T>::reference>
-		std::enable_if_t<!(std::is_pointer<T>::value), std::ostream&>
-		operator<<(std::ostream& xx, const T& it)
-	{
-		if (std::is_const<std::remove_reference_t<S>>::value)
-			std::cout << "cit[";
-		else
-			std::cout << "it[";
-		std::cout << *it;
-		std::cout << "]";
-		return xx;
-	}
+  template<typename T,
+    typename S = typename std::iterator_traits<T>::reference>
+    std::ostream& operator<<(std::ostream& xx, const T& it)
+  {
+    if(std::is_pointer<T>::value)
+    {
+      if(std::is_const<std::remove_reference_t<S>>::value)
+        angleBraces.printElementsPtr(it, "const_pointer");
+      else
+        angleBraces.printElementsPtr(it, "pointer");
+    }
+    else
+    {
+      if(std::is_const<std::remove_reference_t<S>>::value)
+        angleBraces.printElementsPtr(it, "const_iterator");
+      else
+        angleBraces.printElementsPtr(it, "iterator");
+    }
+    return xx;
+  }
 
-	template<typename T,
-		typename S = typename std::iterator_traits<T>::reference>
-		std::enable_if_t<std::is_pointer<T>::value, std::ostream&>
-		operator<<(std::ostream& xx, const T& it)
-	{
-		if (std::is_const<std::remove_reference_t<S>>::value)
-			std::cout << "cptr[";
-		else
-			std::cout << "ptr[";
-		std::cout << *it;
-		std::cout << "]";
-		return xx;
-	}
-
-	// printer for std::array<T, N> : Checked
-	template<typename T,
-		size_t N> std::ostream&
+  template<typename T, size_t N> std::ostream&
 		operator<<(std::ostream& xx, const std::array<T, N>& ar)
 	{
-		std::cout << "arr" << N << "[";
-		for (const auto& x : ar)
-		{
-			typedef typename std::remove_const<
-				typename std::remove_reference<decltype(x)>::type>::type TT;
-			if (std::is_same<std::string, TT>::value)
-				std::cout << "\"" << x << "\",";
-			else
-				std::cout << x << ",";
-		}
-		if (ar.size() > 0)
-			std::cout << "\b";
-		std::cout << "]";
+    boxBraces.printElementsCont(ar, "array", N);
 		return xx;
 	}
 
-	// printer for std::list<T> : Checked
-	template<typename T> std::ostream&
+  template<typename T> std::ostream&
 		operator<<(std::ostream& xx, const std::list<T>& ar)
 	{
-		std::cout << "lst" << ar.size() << "[";
-		for (const auto& x : ar)
-		{
-			typedef typename std::remove_const<
-				typename std::remove_reference<decltype(x)>::type>::type TT;
-			if (std::is_same<std::string, TT>::value)
-				std::cout << "\"" << x << "\",";
-			else
-				std::cout << x << ",";
-		}
-		if (ar.size() > 0)
-			std::cout << "\b";
-		std::cout << "]";
+    curlyBraces.printElementsCont(ar, "list", ar.size());
 		return xx;
 	}
 
-	// printer for std::unordered_set<T> : Checked
-	template<typename T> std::ostream&
+  template<typename T> std::ostream&
 		operator<<(std::ostream& xx, const std::unordered_set<T>& ar)
 	{
-		std::cout << "uset" << ar.size() << "{";
-		for (const auto& x : ar)
-		{
-			typedef typename std::remove_const<
-				typename std::remove_reference<decltype(x)>::type>::type TT;
-			if (std::is_same<std::string, TT>::value)
-				std::cout << "\"" << x << "\",";
-			else
-				std::cout << x << ",";
-		}
-		if (ar.size() > 0)
-			std::cout << "\b";
-		std::cout << "}";
+    boxBraces.printElementsCont(ar, "unordered_set", ar.size());
 		return xx;
 	}
 
-	// printer for std::set<T>
-	template<typename T> std::ostream&
+  template<typename T> std::ostream&
 		operator<<(std::ostream& xx, const std::set<T>& ar)
 	{
-		std::cout << "set" << ar.size() << "{";
-		for (const auto& x : ar)
-		{
-			typedef typename std::remove_const<
-				typename std::remove_reference<decltype(x)>::type>::type TT;
-			if (std::is_same<std::string, TT>::value)
-				std::cout << "\"" << x << "\",";
-			else
-				std::cout << x << ",";
-		}
-		if (ar.size() > 0)
-			std::cout << "\b";
-		std::cout << "}";
+    boxBraces.printElementsCont(ar, "set", ar.size());
 		return xx;
 	}
 
-	// printer for std::unordered_multiset<T>
-	template<typename T> std::ostream&
-		operator<<(std::ostream& xx,
-			const std::unordered_multiset<T>& ar)
+  template<typename T> std::ostream&
+		operator<<(std::ostream& xx, const std::unordered_multiset<T>& ar)
 	{
-		std::cout << "umset" << ar.size() << "{";
-		for (const auto& x : ar)
-		{
-			typedef typename std::remove_const<
-				typename std::remove_reference<decltype(x)>::type>::type TT;
-			if (std::is_same<std::string, TT>::value)
-				std::cout << "\"" << x << "\",";
-			else
-				std::cout << x << ",";
-		}
-		if (ar.size() > 0)
-			std::cout << "\b";
-		std::cout << "}";
+    boxBraces.printElementsCont(ar, "unordered_multiset", ar.size());
 		return xx;
 	}
 
-	// printer for std::multiset<T>
-	template<typename T> std::ostream&
+  template<typename T> std::ostream&
 		operator<<(std::ostream& xx, const std::multiset<T>& ar)
 	{
-		std::cout << "mset" << ar.size() << "{";
-		for (const auto& x : ar)
-		{
-			typedef typename std::remove_const<
-				typename std::remove_reference<decltype(x)>::type>::type TT;
-			if (std::is_same<std::string, TT>::value)
-				std::cout << "\"" << x << "\",";
-			else
-				std::cout << x << ",";
-		}
-		if (ar.size() > 0)
-			std::cout << "\b";
-		std::cout << "}";
+    boxBraces.printElementsCont(ar, "multiset", ar.size());
 		return xx;
 	}
 
-	// printer for std::map<T1, T2> : Checked
-	template<typename T1, typename T2> std::ostream&
+  template<typename T1, typename T2> std::ostream&
 		operator<<(std::ostream& xx, const std::map<T1, T2>& ar)
 	{
-		std::cout << "map" << ar.size() << "[";
-		for (const auto& x : ar)
-			std::cout << x << ",";
-		if (ar.size() > 0)
-			std::cout << "\b";
-		std::cout << "]";
+    boxBraces.printElementsCont(ar, "map", ar.size());
 		return xx;
 	}
 
-	// printer for std::unordered_map<T1, T2>
-	template<typename T1, typename T2> std::ostream&
+  template<typename T1, typename T2> std::ostream&
 		operator<<(std::ostream& xx, const std::unordered_map<T1, T2>& ar)
 	{
-		std::cout << "umap" << ar.size() << "[";
-		for (const auto& x : ar)
-			std::cout << x << ",";
-		if (ar.size() > 0)
-			std::cout << "\b";
-		std::cout << "]";
+    boxBraces.printElementsCont(ar, "unordered_map", ar.size());
 		return xx;
 	}
 
-	// printer for std::multimapmap<T1, T2>
-	template<typename T1, typename T2> std::ostream&
+  template<typename T1, typename T2> std::ostream&
 		operator<<(std::ostream& xx, const std::multimap<T1, T2>& ar)
 	{
-		std::cout << "mmap" << ar.size() << "[";
-		for (const auto& x : ar)
-			std::cout << x << ",";
-		if (ar.size() > 0)
-			std::cout << "\b";
-		std::cout << "]";
+    boxBraces.printElementsCont(ar, "multimap", ar.size());
 		return xx;
 	}
 
-	// printer for std::unordered_multimap<T1, T2>
-	template<typename T1, typename T2> std::ostream&
-		operator<<(std::ostream& xx,
-			const std::unordered_multimap<T1, T2>& ar)
+  template<typename T1, typename T2> std::ostream&
+		operator<<(std::ostream& xx, const std::unordered_multimap<T1, T2>& ar)
 	{
-		std::cout << "ummap" << ar.size() << "[";
-		for (const auto& x : ar)
-			std::cout << x << ",";
-		if (ar.size() > 0)
-			std::cout << "\b";
-		std::cout << "]";
+    boxBraces.printElementsCont(ar, "unordered_multimap", ar.size());
 		return xx;
 	}
 
-	// printer for std::shared_ptr<T>
-	template<typename T> std::ostream&
+  template<typename T> std::ostream&
 		operator<<(std::ostream& xx,
 			const std::shared_ptr<T>& sp)
 	{
-		std::cout << "sp[" << *sp << "]";
+    angleBraces.printElementsPtr(sp, "shared_ptr");
 		return xx;
 	}
 
-	// printer for std::unique_ptr<T>
-	template<typename T> std::ostream&
+  template<typename T> std::ostream&
 		operator<<(std::ostream& xx,
 			const std::unique_ptr<T>& up)
 	{
-		std::cout << "up[" << *up << "]";
+    angleBraces.printElementsPtr(up, "unique_ptr");
 		return xx;
 	}
 }
